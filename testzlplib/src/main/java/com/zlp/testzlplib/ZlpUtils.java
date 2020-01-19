ac8827b7f6180ba66c0f03398a6637ca310eddb9
zlp18300076338 <yzlpyab@163.com>
2020-01-19 14:29:52 +0800

    Initial commit

---
+++ testzlplib/src/main/java/com/zlp/testzlplib/ZlpUtils.java
@@ -0,0 +1,15 @@
+package com.zlp.testzlplib;
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * Created by zlp on 2020/1/19 0019.
+ */
+public class ZlpUtils {
+
+    public static void toSubActivity(Activity activity){
+        Intent intent = new Intent();
+        activity.startActivity(intent);
+    }
+}
